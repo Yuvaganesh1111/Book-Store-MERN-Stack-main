0935089a06c1a2f78b3c05c9862742d273993957
Yuvaganesh1111 <yuvaganeshyg1111@gmail.com>
2023-11-03 19:07:33 +0530

    update

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 export const PORT = 5555;
 
-export const mongoDBURL ='mongodb+srv://root:yuva@book-store.4yaw6i5.mongodb.net/bookstore?retryWrites=true&w=majority&appName=AtlasApp';
+export const mongoDBURL ='mongodbAtlasApp';
 
 
 // Please create a free database for yourself.
